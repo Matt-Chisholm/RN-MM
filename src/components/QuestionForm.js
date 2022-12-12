6095ef2384391fd7a925e84e13a59bb349e192f9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-12 11:16:08 -0800

    centering and component

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function QuestionForm() {
+  return (
+    <View>
+      <Text>QuestionForm</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/screens/SafetyFourScreen.js
+++ src/screens/SafetyFourScreen.js
@@ -3,10 +3,16 @@ import React from "react";
 
 export default function SafetyFourScreen() {
   return (
-    <View>
+    <View style={container}>
       <Text>SafetyFourScreen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
