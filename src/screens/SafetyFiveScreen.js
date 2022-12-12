723b1143eb1a1cd8d0ea678683c296afef1bcba9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-12 11:06:31 -0800

    boilerplate

---
+++ src/screens/SafetyFiveScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function SafetyFiveScreen() {
+  return (
+    <View>
+      <Text>SafetyFiveScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/screens/SafetyThreeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function SafetyThreeScreen() {
+  return (
+    <View>
+      <Text>SafetyThreeScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
