960276515044013a729bd2b9e1d7916ae2c3d3bc
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-12 12:30:33 -0800

    filling out form

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,12 +1,60 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import React from "react";
+import { Feather } from "@expo/vector-icons";
 
-export default function QuestionForm() {
+export default function QuestionForm({
+  navigation,
+  header,
+  subheader,
+  question,
+  nextScreen,
+  prevScreen,
+}) {
   return (
-    <View>
-      <Text>QuestionForm</Text>
+    <View style={styles.container}>
+      <View>
+        <Feather name='x' size={40} color='black' style={styles.icon} />
+      </View>
+      <Text style={styles.header}>{header}</Text>
+      <Text style={styles.subheader}>{subheader}</Text>
+      <Text style={styles.question}>{question}</Text>
+      <TextInput style={styles.input} />
+      <Button title='Next' onPress={nextScreen} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+    alignItems: "center",
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 20,
+    padding: 10,
+  },
+  icon: {
+    paddingLeft: 300,
+  },
+  header: {
+    fontSize: 35,
+    fontWeight: "bold",
+    paddingRight: 100,
+  },
+  subheader: {
+    fontSize: 30,
+    fontWeight: "bold",
+    paddingRight: 100,
+    marginTop: 30,
+  },
+  question: {
+    fontSize: 16,
+    paddingRight: 100,
+    marginTop: 20,
+  },
+});
--- src/screens/SafetyOneScreen.js
+++ src/screens/SafetyOneScreen.js
@@ -1,12 +1,18 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import QuestionForm from "../components/QuestionForm";
 
-export default function SafetyOneScreen() {
+export default function SafetyOneScreen({ navigation }) {
   return (
     <LinearGradient colors={["#87dcff", "#6043ad"]} style={styles.gradient}>
       <View style={styles.container}>
-        <Text>SafetyOneScreen</Text>
+        <QuestionForm
+          header="Let's start"
+          subheader='My Triggers'
+          question='What are my top triggers or stresses?'
+          nextScreen={() => navigation.navigate("SafetyTwo")}
+        />
       </View>
     </LinearGradient>
   );
@@ -15,7 +21,7 @@ export default function SafetyOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    marginTop: 50,
     alignItems: "center",
   },
   gradient: {
