cc2e211cfe23a4430c644b0aadfc27d4db20a2fd
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-16 12:27:57 -0800

    displaying results

--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -1,10 +1,23 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import { Context as SafetyContext } from "../context/SafetyContext";
 
 export default function ResultsScreen() {
+  const { state } = useContext(SafetyContext);
+
   return (
     <View style={styles.container}>
-      <Text>ResultsScreen</Text>
+      <Text style={styles.name}>{state.planName}</Text>
+      <Text style={styles.header}>My triggers</Text>
+      <Text>{state.triggers}</Text>
+      <Text style={styles.header}>My warning signs</Text>
+      <Text>{state.warnings}</Text>
+      <Text style={styles.header}>My coping and distraction strategies</Text>
+      <Text>{state.strategies}</Text>
+      <Text style={styles.header}>My supports</Text>
+      <Text>{state.supports}</Text>
+      <Text style={styles.header}>My codeword</Text>
+      <Text>{state.codewords}</Text>
     </View>
   );
 }
@@ -14,5 +27,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
 });
